e30a82759da79e896fdd9b8724a441249e8dfa00
jonschlinkert <dev@sellside.com>
2014-09-24 02:53:27 -0400

    move fixtures to root

---
+++ test.js
@@ -0,0 +1,28 @@
+/*!
+ * extract-comments <https://github.com/jonschlinkert/extract-comments>
+ *
+ * Copyright (c) 2014 Jon Schlinkert, contributors.
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+var assert = require('assert');
+var should = require('should');
+var extract = require('./');
+
+describe('.extract():', function () {
+  it('should read each file as a string and extract comments from the code.', function () {
+    var actual = extract.fromFiles('fixtures/**/*.js');
+
+    actual.should.be.an.object;
+    actual.should.have.property('fixtures/assemble.js');
+    actual['fixtures/assemble.js'].should.have.property('122',  {
+      type: 'comment',
+      comment: ' * Initialize default configuration.\n *\n * @api private\n ',
+      begin: 122,
+      end: 125
+    });
+  });
+});
+
