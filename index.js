36b575dd744402139463cdb35badf7775df0c70c
jonschlinkert <dev@sellside.com>
2014-09-24 02:54:55 -0400

    v0.4.0
    - no longer uses esprima because of how slow it was
    - comment key is now the line number
    - strings are now parsed by default
    - `.fromFile()` must be used to parse files/globs

--- index.js
+++ index.js
@@ -8,39 +8,9 @@
 'use strict';
 
 var fs = require('fs');
-var esprima = require('esprima');
 var mapFiles = require('map-files');
-var _ = require('lodash');
-
-
-/**
- * Extract code comments from a glob of files:
- *
- * **Example:**
- *
- * ```js
- * var extract = require('extract-comments');
- * extract('lib/*.js');
- * ```
- *
- * @param  {String} `patterns` Glob patterns to used.
- * @param  {Object} `options` Options to pass to [esprima] or [globby], or [map-files].
- * @return {Object} Object of code comments.
- * @api public
- */
-
-function extract(patterns, options) {
-  return mapFiles(patterns, _.extend({
-    // noop for mapFiles
-    rename: function(filepath) {
-      return filepath;
-    },
-    parse: function (filepath, options) {
-      var code = fs.readFileSync(filepath, 'utf8');
-      return extract.fromString(code, options);
-    }
-  }, options));
-}
+var lineCount = require('line-count');
+var extend = require('mixin-deep');
 
 
 /**
@@ -50,18 +20,67 @@ function extract(patterns, options) {
  *
  * ```js
  * var extract = require('extract-comments');
- * extract.fromString('// this is a code comment');
+ * extract('// this is a code comment');
  * ```
  *
  * @param  {String} `string`
- * @param  {Object} `options` Options to pass to esprima.
  * @return {Object} Object of code comments.
  * @api public
  */
 
-extract.fromString = function(string, options) {
-  var opts = _.extend({comment: true, loc: true }, options);
-  return esprima.parse(string, opts).comments;
+function extract(str) {
+  var match, o = {};
+  var line = 1;
+
+  while (match = (/\/\*{1,2}[^\*!]([\s\S]*?)\*\//g).exec(str)) {
+    var start = str;
+
+    // add lines from before the comment
+    line += lineCount(start.substr(0, match.index)) - 1;
+
+    // Update the string
+    str = str.substr(match.index + match[1].length);
+
+    o[line] = {
+      type: 'comment',
+      comment: match[1],
+      begin: line,
+      end: line + lineCount(match[1]) - 1
+    };
+
+    // add lines from the comment itself
+    line += lineCount(start.substr(match.index, match[1].length)) - 1;
+  }
+  return o;
+}
+
+
+/**
+ * Extract code comments from a file or glob of files:
+ *
+ * **Example:**
+ *
+ * ```js
+ * var extract = require('extract-comments');
+ * extract.fromFiles(['lib/*.js']);
+ * ```
+ *
+ * @param  {String} `patterns` Glob patterns to used.
+ * @param  {Object} `options` Options to pass to [globby], or [map-files].
+ * @return {Object} Object of code comments.
+ * @api public
+ */
+
+extract.fromFiles = function(patterns, options) {
+  return mapFiles(patterns, extend({
+    rename: function(filepath) {
+      return filepath;
+    },
+    parse: function (filepath, options) {
+      var code = fs.readFileSync(filepath, 'utf8');
+      return extract(code, options);
+    }
+  }, options));
 };
 
 
