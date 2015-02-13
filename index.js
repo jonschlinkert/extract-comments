eecf9783c1872f9be99a4be36092b3a581e4b6be
jonschlinkert <jon.schlinkert@sellside.com>
2015-02-13 13:00:59 -0500

    refactor

--- index.js
+++ index.js
@@ -9,20 +9,15 @@
 
 var fs = require('fs');
 var mapFiles = require('map-files');
-var lineCount = require('line-count');
-var extend = require('mixin-deep');
-var extractComments = require('./lib/extract');
 
 /**
- * Expose `extract`
+ * expose `extract`
  */
 
 module.exports = extract;
 
 /**
  * Extract code comments from the given `string`.
- *
- * **Example:**
  *
  * ```js
  * var extract = require('extract-comments');
@@ -34,20 +29,65 @@ module.exports = extract;
  * @api public
  */
 
-function extract(str, opts) {
-  opts = opts || {};
+function extract(str, fn) {
+  var start = /^\s*\/\*\*?/;
+  var middle = /^\s*\*([^\n*]*)/;
+  var end = /^\s*\*\//;
 
-  return extractComments(str, opts).reduce(function(acc, obj) {
-    // obj.type = 'comment';
-    // acc[obj.begin] = obj;
-    return acc;
-  }, {});
-}
+  var lines = str.split('\n');
+  var len = lines.length, i = 0, m;
+  var comments = {}, o = {};
+  var isComment = false, afterCount;
+
+  while (i < len) {
+    var line = lines[i++];
+
+    if (!isComment && start.test(line)) {
+      afterCount = 0;
+      isComment = true;
+      o = {};
+      o.begin = i;
+      o.comment = '';
+      o.after = '';
+    }
+
+    if (isComment && end.test(line)) {
+      o.end = i;
+      o.type = 'comment';
+      comments[o.begin] = o;
+      isComment = false;
+    }
+
+    if (isComment && i > o.begin) {
+      m = middle.exec(line);
+      if (m) line = m[1];
+
+      o.comment += (line || '').trim() + '\n';
+    }
+
+    if (!isComment && o.end && i > o.end && afterCount < 2) {
+      o.after += line + '\n';
+      afterCount++;
+    }
+
+    if (o.begin && o.after !== '') {
+      o.after = o.after.trim().split('\n')[0];
+
+      // callback
+      if (typeof fn === 'function') {
+        comments[o.begin] = fn(comments[o.begin]);
+      }
+    }
+  }
+  return comments;
+};
 
 /**
- * Extract code comments from a file or glob of files:
+ * Extract code comments from a file or glob of files.
+ * You may also pass a custom `rename` function on the options
+ * to change the key of each object returned.
  *
- * **Example:**
+ * See [map-files] for more details and available options.
  *
  * ```js
  * var extract = require('extract-comments');
@@ -60,14 +100,14 @@ function extract(str, opts) {
  * @api public
  */
 
-extract.fromFiles = function(patterns, options) {
-  return mapFiles(patterns, extend({
-    rename: function(filepath) {
-      return filepath;
-    },
-    parse: function (filepath, options) {
-      var code = fs.readFileSync(filepath, 'utf8');
-      return extract(code, options);
-    }
-  }, options));
+extract.fromFiles = function(patterns, opts) {
+  opts = opts || {};
+  opts.name = opts.rename || function(fp) {
+    return fp;
+  };
+  opts.read = opts.read || function(fp, options) {
+    var code = fs.readFileSync(fp, 'utf8');
+    return extract(code, options);
+  };
+  return mapFiles(patterns, opts);
 };
