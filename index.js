495d2c711c6afe7d16a817d22ab644ebb3303f55
jonschlinkert <jon.schlinkert@sellside.com>
2015-11-04 03:49:37 -0500

    minor refactor, add code comments

--- index.js
+++ index.js
@@ -4,147 +4,117 @@ var extend = require('extend-shallow');
 var Block = require('./lib/block');
 var Line = require('./lib/line');
 var utils = require('./lib/utils');
-var cache = {};
 
 /**
- * Get block comments from the given string
+ * Extract comments from the given `string`.
+ *
+ * ```js
+ * extract(str, options);
+ * ```
+ * @param {String} `string`
+ * @param {Object} `options` Pass `first: true` to return after the first comment is found.
+ * @return {String}
+ * @api public
  */
 
-function comments(str, fn) {
+function comments(str, options, fn) {
   if (typeof str !== 'string') {
     throw new TypeError('expected a string');
   }
-
-  var block = blockComments(str, fn);
-  var line = lineComments(str, fn);
-  return block.concat(line).sort(function(a, b) {
-    return a.loc.start.pos - b.loc.start.pos;
-  });
+  return block(str, options, fn)
+    .concat(line(str, options, fn))
+    .sort(compare);
 }
 
 /**
- * Get block and line comments from the given string
+ * Extract block comments from the given `string`.
+ *
+ * ```js
+ * extract.block(str, options);
+ * ```
+ * @param {String} `string`
+ * @param {Object} `options` Pass `first: true` to return after the first comment is found.
+ * @return {String}
+ * @api public
  */
 
-function blockComments(str, options, fn) {
-  if (typeof str !== 'string') {
-    throw new TypeError('expected a string');
-  }
-
-  if (typeof options === 'function') {
-    fn = options;
-    options = {};
-  }
-
-  if (typeof fn !== 'function') {
-    fn = utils.identity;
-  }
-
-  var opts = extend({}, options);
-  str = utils.normalize(str);
-  str = utils.escapeQuoted(str, cache);
-
-  var arr = [];
-  var start = findStart('/*');
-  var end = findEnd('*/');
-  var len = str.length;
-  var startIdx = start(str, 0);
-  var endIdx = 0;
-
-  while (startIdx !== -1) {
-    endIdx = end(str, startIdx, len);
-    if (endIdx === -1) {
-      endIdx = len;
-    }
-
-    var comment = fn(new Block(str, startIdx, endIdx, cache));
-    arr.push(comment);
-    if (opts.first && arr.length === 1) {
-      return arr;
-    }
-
-    if (comment.code.value) {
-      endIdx += comment.code.value.length;
-    }
-    startIdx = start(str, endIdx + 2);
-  }
-  return arr;
+function block(str, options, fn) {
+  return factory('/*', '*/', Block)(str, options, fn);
 }
 
 /**
- * Get line comments from the given string
+ * Extract line comments from the given `string`.
+ *
+ * ```js
+ * extract.line(str, options);
+ * ```
+ * @param {String} `string`
+ * @param {Object} `options` Pass `first: true` to return after the first comment is found.
+ * @return {String}
+ * @api public
  */
 
-function lineComments(str, options, fn) {
-  if (typeof str !== 'string') {
-    throw new TypeError('expected a string');
-  }
+function line(str, options, fn) {
+  return factory('//', '\n', Line)(str, options, fn);
+}
 
-  str = utils.normalize(str);
-  str = utils.escapeQuoted(str, cache);
-  var comments = [];
+/**
+ * Factory for extracting comments from a string.
+ *
+ * @param {String} `string`
+ * @return {String}
+ */
 
-  if (typeof options === 'function') {
-    fn = options;
-    options = {};
-  }
-
-  if (typeof fn !== 'function') {
-    fn = utils.identity;
-  }
-
-  var opts = extend({}, options);
-  var combine = opts.combine === true;
-  var start = findStart('//');
-  var end = findEnd('\n');
-  var len = str.length;
-  var startIdx = start(str, 0);
-  var endIdx = 0, prev;
-  var stacked = null;
-
-  while (startIdx !== -1) {
-    if (startIdx >= len) {
-      break;
+function factory(open, close, Ctor) {
+  return function(str, options, fn) {
+    if (typeof str !== 'string') {
+      throw new TypeError('expected a string');
     }
 
-    endIdx = end(str, startIdx, len);
-    if (endIdx === -1 || endIdx > len) {
-      endIdx = len;
+    if (typeof options === 'function') {
+      fn = options;
+      options = {};
     }
 
-    var comment = new Line(str, startIdx, endIdx);
-    startIdx = start(str, endIdx);
+    if (typeof fn !== 'function') {
+      fn = utils.identity;
+    }
 
-    if (prev && combine && isStacked(comment, prev, opts, cache)) {
-      var curr = comment.loc.end.line;
-      var last = comments[comments.length - 1];
-      prev = merge(str, comment, prev);
-      if (last && prev.start === last.start) {
-        comments.pop();
+    var opts = extend({}, options);
+    str = utils.normalize(str);
+    str = utils.escapeQuoted(str);
+
+    var res = [];
+    var start = str.indexOf(open);
+    var end = str.indexOf(close, start);
+    var len = str.length;
+    if (end === -1) {
+      end = len;
+    }
+
+    while (start !== -1 && end <= len) {
+      var comment = fn(new Ctor(str, start, end, open, close));
+      res.push(comment);
+      if (opts.first && res.length === 1) {
+        return res;
       }
-      prev.loc.start.line = curr;
-      stacked = prev;
-      continue;
+      start = str.indexOf(open, end + 1);
+      end = str.indexOf(close, start);
+      if (end === -1) {
+        end = len;
+      }
     }
-
-    if (stacked) {
-      comments.push(fn(stacked));
-      stacked = null;
-    }
-
-    comment = fn(comment);
-    prev = comment;
-    comments.push(comment);
-  }
-
-  if (stacked) {
-    comments.push(fn(stacked));
-  }
-  return comments;
+    return res;
+  };
 }
 
 /**
- * Get the first block comment from the given string
+ * Extract the first comment from the given `string`.
+ *
+ * @param {String} `string`
+ * @param {Object} `options` Pass `first: true` to return after the first comment is found.
+ * @return {String}
+ * @api public
  */
 
 function first(str) {
@@ -152,74 +122,21 @@ function first(str) {
     throw new TypeError('expected a string');
   }
 
-  str = utils.normalize(str);
-  if (!/^\/\*{1,2}!?/.test(str)) {
+  var arr = comments(str, {first: true});
+  if (arr && arr.length) {
+    return arr[0].raw;
+  } else {
     return null;
   }
-  var i = str.indexOf('*/');
-  if (i === -1) return null;
-  if (/['"\w]/.test(str[i + 2])) {
-    return null;
-  }
-  return str.slice(0, i + 2);
 }
 
 /**
- * Returns a function for getting the index the
- * given "end" character(s)
- *
- * @param {String} endChars
+ * Utility for sorting line and block comments into
+ * the correct order.
  */
 
-function findStart(startChars) {
-  return function(str, idx) {
-    var i = str.indexOf(startChars, idx);
-    var prev = str[i - 1];
-    if (prev && /['"\w]/.test(prev)) {
-      i = str.indexOf(startChars, i + 1);
-      prev = str[i - 1];
-    }
-    return i;
-  };
-}
-
-/**
- * Returns a function for getting the index the
- * given "end" character(s)
- *
- * @param {String} endChars
- */
-
-function findEnd(endChars) {
-  return function(str, start) {
-    var idx = str.indexOf(endChars, start + 2);
-    var ch = str[idx + 2];
-    while (ch && /['"]/.test(ch)) {
-      idx = str.indexOf(endChars, idx + 2);
-      ch = str[idx + 2];
-    }
-    return idx;
-  };
-}
-
-/**
- * Returns true if the previous line was a line comment.
- */
-
-function isStacked(comment, prev) {
-  var prevLine = prev.loc.start.line;
-  var line = comment.loc.end.line;
-  return line === prevLine + 1;
-}
-
-/**
- * Merge line comments
- */
-
-function merge(str, curr, prev) {
-  var i = prev.loc.start.pos;
-  var end = curr.loc.end.pos;
-  return new Line(str, i, end);
+function compare(a, b) {
+  return a.loc.start.pos - b.loc.start.pos;
 }
 
 /**
@@ -238,10 +155,16 @@ module.exports.first = first;
  * Expose `extract.block` method
  */
 
-module.exports.block = blockComments;
+module.exports.block = block;
 
 /**
  * Expose `extract.line` method
  */
 
-module.exports.line = lineComments;
+module.exports.line = line;
+
+/**
+ * Expose `extract.factory` method
+ */
+
+module.exports.factory = factory;
